0c9b273529596607bba4d1566b6a76b0cb409a9a
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-09-07 12:07:46 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Speech/EntitySystems/ReplacementAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/ReplacementAccentSystem.cs
@@ -10,6 +10,7 @@
 // SPDX-FileCopyrightText: 2024 Tayrtahn <tayrtahn@gmail.com>
 // SPDX-FileCopyrightText: 2024 lzk <124214523+lzk228@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 SX_7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 Winkarst <74284083+Winkarst-cpu@users.noreply.github.com>
