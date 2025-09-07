e0f8d6a6a5c6b7a2aa2e4a3b5685860be71daa7a
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-09-07 11:55:42 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Buckle/SharedBuckleSystem.Buckle.cs
+++ Content.Shared/Buckle/SharedBuckleSystem.Buckle.cs
@@ -93,6 +93,7 @@
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2024 Арт <123451459+JustArt1m@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
