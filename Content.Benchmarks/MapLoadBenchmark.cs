7a588ad112cdbbd3365ce8db80a882ac32fff689
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-09-10 22:58:26 +0000

    chore: Automatically update REUSE headers

--- Content.Benchmarks/MapLoadBenchmark.cs
+++ Content.Benchmarks/MapLoadBenchmark.cs
@@ -20,6 +20,7 @@
 // SPDX-FileCopyrightText: 2025 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 OnsenCapy <101037138+OnsenCapy@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <92227810+SX-7@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Sera <karaadastra@gmail.com>
 // SPDX-FileCopyrightText: 2025 Spessmann <156740760+Spessmann@users.noreply.github.com>
